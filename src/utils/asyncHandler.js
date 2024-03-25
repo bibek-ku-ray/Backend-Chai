c54c3a7bd72d47504214284ca8ed6deb3443af19
bibek-ku-ray <bi.ray1225@gmail.com>
2024-03-26 01:11:23 +0545

    util code, api response and error

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch(() => next(err));
+    };
+};
+
+export { asyncHandler };
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+*/
