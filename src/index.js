b81843cdb0cb1ded78af0bb6a57bea304140c333
bibek-ku-ray <bi.ray1225@gmail.com>
2024-03-26 01:10:02 +0545

    util code api response and error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import express from "express"
 
 dotenv.config({
     path: "./env"
 })
 
+const app = express()
+
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8001, () => {
+        console.log(`Server running on PORT: ${process.env.PORT}`);
+    })
+    app.on("error", (error) => {
+        console.log("ERR: ", error);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB CONNECTION FAILED !!! ", error);
+})
