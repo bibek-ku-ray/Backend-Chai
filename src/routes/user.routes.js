f88dcd8e34ef94588fb8e7231e88868c17fd74e1
bibek-ku-ray <bi.ray1225@gmail.com>
2024-03-29 17:51:56 +0545

    Add router for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessTokens } from "../controllers/user.controller.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessTokens, 
+    changePassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +38,41 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessTokens)
+
+router.route("/change-password")
+    .post(
+        verifyJWT, 
+        changePassword
+    )
+
+router.route('/current-user')
+    .get(
+        verifyJWT, 
+        getCurrentUser
+    )
+
+router.route("/update-account")
+    .patch(
+        verifyJWT, 
+        updateAccountDetails
+    )
+
+router.route("/avatar")
+    .patch(
+        verifyJWT, 
+        upload.single("avatar"), 
+        updateUserAvatar
+    )
+
+router.route("/cover-image")
+    .patch(
+        verifyJWT, 
+        upload.single("/coverImage"), 
+        updateUserCoverImage
+    )
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router;
